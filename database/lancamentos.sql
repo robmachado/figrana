ecd8b7a7a6303dc0e770055eb39bfa2042a44be9
robmachado <linux.rlm@gmail.com>
2018-05-16 17:23:10 -0300

    [UPD] +17

--- database/lancamentos.sql
+++ database/lancamentos.sql
@@ -1,4 +1,3 @@
-
 -- CREATE DATABASE figrana CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 USE figrana;
@@ -6,42 +5,15 @@ USE figrana;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
---
--- Database: `figrana`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `lancamentos`
---
-
 CREATE TABLE `lancamentos` (
   `id` int(11) NOT NULL,
   `chave` varchar(44) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `lancamentos`
---
 ALTER TABLE `lancamentos`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `chave` (`chave`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `lancamentos`
---
 ALTER TABLE `lancamentos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
