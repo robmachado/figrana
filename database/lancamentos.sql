4a0ab85df9a035c29fe4dabb5437e8c1d96491cb
robmachado <linux.rlm@gmail.com>
2018-05-16 17:20:42 -0300

    [UPD] =17

--- database/lancamentos.sql
+++ database/lancamentos.sql
@@ -1,22 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: 09-Maio-2018 às 10:52
--- Versão do servidor: 5.5.60-0ubuntu0.14.04.1
--- PHP Version: 7.0.30-1+ubuntu14.04.1+deb.sury.org+1
 
-use figrana;
+-- CREATE DATABASE figrana CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE figrana;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `figrana`
